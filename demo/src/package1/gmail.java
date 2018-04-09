1fec22ef64110537a34df6b863649e753a7fe46d
Supreme <Supreme@DESKTOP-TNBFE61>
2018-04-09 12:09:55 +0530

    created automation demo project

---
+++ demo/src/package1/gmail.java
@@ -0,0 +1,23 @@
+package package1;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class gmail {
+	
+	
+	@Test
+	public void Testcases(){
+		WebDriver d1=new FirefoxDriver();
+		d1.get("http:\\www.google.com");
+		d1.get("http:\\www.facebook.com");
+		d1.findElement(By.id("email")).sendKeys("supreme@gmaiul.com");
+		d1.findElement(By.id("pass")).sendKeys("123456");
+		d1.quit();
+		
+
+	}
+
+}
